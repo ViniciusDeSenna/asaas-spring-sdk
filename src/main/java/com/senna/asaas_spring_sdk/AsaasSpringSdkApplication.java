b4e2cf4624bed5499bdf55bdeeaf7906f62aa6b3
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2025-04-23 13:02:59 -0300

    Spring Init

---
+++ src/main/java/com/senna/asaas_spring_sdk/AsaasSpringSdkApplication.java
@@ -0,0 +1,13 @@
+package com.senna.asaas_spring_sdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsaasSpringSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsaasSpringSdkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senna/asaas_spring_sdk/AsaasSpringSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senna.asaas_spring_sdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsaasSpringSdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
