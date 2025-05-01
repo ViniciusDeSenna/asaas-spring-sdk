4f76eff28a3af079b62273d13ca9212861cfb172
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2025-05-01 17:22:03 -0300

    AsaasCreditCardBrands

--- src/main/java/com/senna/asaas_spring_sdk/credit_card/dto/AsaasCreditCardSummary.java
+++ src/main/java/com/senna/asaas_spring_sdk/credit_card/dto/AsaasCreditCardSummary.java
@@ -1,6 +1,7 @@
 package com.senna.asaas_spring_sdk.credit_card.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.senna.asaas_spring_sdk.credit_card.enums.AsaasCreditCardBrands;
 import lombok.Data;
 
 /**
@@ -18,7 +19,7 @@ public class AsaasCreditCardSummary {
     /**
      * Bandeira do cartão utilizado | Brand of the card used - (VISA, MASTERCARD, ELO, DINERS, DISCOVER, AMEX, HIPERCARD, CABAL, BANESCARD, CREDZ, SOROCRED, CREDSYSTEM, JCB, UNKNOWN)
      */
-    String creditCardBrand;
+    AsaasCreditCardBrands creditCardBrand;
 
     /**
      * Token do cartão de crédito que poderá ser enviado nas próximas transações sem a necessidade de informar novamente os dados de cartão e do titular | Credit card token that can be sent in future transactions without the need to re-enter card and cardholder details.
---
+++ src/main/java/com/senna/asaas_spring_sdk/credit_card/enums/AsaasCreditCardBrands.java
@@ -0,0 +1,18 @@
+package com.senna.asaas_spring_sdk.credit_card.enums;
+
+public enum AsaasCreditCardBrands {
+    VISA,
+    MASTERCARD,
+    ELO,
+    DINERS,
+    DISCOVER,
+    AMEX,
+    HIPERCARD,
+    CABAL,
+    BANESCARD,
+    CREDZ,
+    SOROCRED,
+    CREDSYSTEM,
+    JCB,
+    UNKNOWN
+}
